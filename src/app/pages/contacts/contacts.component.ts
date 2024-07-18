768803672de5b5d45497609b05f879daf2a505f3
brainstrom226 <brainstrom226@gmail.com>
2024-07-18 20:01:12 +0530

    added nd destroy in contacts

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { ContactsService } from '../../services/contacts.service';
+import { Contacts } from '../../models/Contacts';
 
 @Component({
   selector: 'app-contacts',
@@ -18,43 +20,49 @@ export class ContactsComponent {
   userId = '1234';
   showDetails = false;
   constructor(private route: ActivatedRoute, private router: Router, private contactsService:ContactsService) {
-  
-   }
+  }
+  contactsSubscription: Subscription | undefined;
+
+   contacts: Contacts[] = [
+    new Contacts('John Doe', 1234567890, 9876543210),
+    new Contacts('Jane Smith', 1122334455, 5566778899)
+  ];
 
    ngOnInit(): void {
     console.log('fetch contacts details..');
-    this.contactsService.getContacts(this.userId).subscribe(data => {
+    this.contactsSubscription = this.contactsService.getContacts(this.userId).subscribe(data => {
       this.newContacts = data;
     });
    }
 
   submitForm() {
     console.log('contacts saved..');
-    this.contactsService.addContacts(this.newContacts).subscribe((res) => {
+    this.contactsSubscription = this.contactsService.addContacts(this.newContacts).subscribe((res) => {
       console.log(res);
     })
   }
 
   update() {
     console.log('contacts updated..');
-    this.contactsService.updateContacts(this.newContacts).subscribe((res) => {
+    this.contactsSubscription =this.contactsService.updateContacts(this.newContacts).subscribe((res) => {
       console.log(res);
     })
   }
 
   delete() {
     console.log('contacts deleted..');
-    this.contactsService.deleteContacts(this.newContacts).subscribe((res) => {
+    this.contactsSubscription = this.contactsService.deleteContacts(this.newContacts).subscribe((res) => {
       console.log(res);
     })
   }
 
-  getContacts() {
-    console.log('fetch contact details..');
-
-  }
-
   onAddClick() {
     this.showDetails = true;
+  }
+
+  ngOnDestroy() {
+    if(this.contactsSubscription){
+      this.contactsSubscription.unsubscribe();
+    }
   }
 }
